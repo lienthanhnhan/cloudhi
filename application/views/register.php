665c6a5a366d50f242a22e0ecfd91a31a1c37068
root <root@webrtc.pbxtel.net>
2017-06-05 11:15:53 +0700

    add controller/view register

---
+++ application/views/register.php
@@ -0,0 +1,125 @@
+<style type="text/css">
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	#body {
+		margin: 0 15px 70px 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+		height: 60%;
+		width: 60%;
+	}
+</style>
+<div id="container">
+	<h3 style="color: #fff; background-color: #6a7fef; padding: 15px 0 15px 10px; border-bottom: 1px solid #D0D0D0;">Đăng ký sử dụng dịch vụ</h3>
+
+	<div id="body">
+		<h4>Thông tin công ty</h4>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Công ty: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Mã số thuế: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Địa chỉ: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Số điện thoại: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+
+		<h4>Thông tin người đại diện</h4>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Họ tên: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Chức vụ: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Điện thoại #1: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Điện thoại #2: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+
+		<h4>Thông tin dịch vụ</h4>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Địa chỉ mail: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Mật khẩu: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Nhập lại mật khẩu: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+		<div class="form-group row">
+  			<label class="col-2 col-form-label">Domain: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+  		<div class="form-group row">
+  			<label class="col-2 col-form-label">SĐT liên hệ: </label>
+  			<div class="col-10">
+	    		<input class="form-control" type="text">
+  			</div>
+  		</div>
+  		<div class="form-group" style="float: right;">
+	  		<button type="button" class="btn">Cancel</button>
+	  		<button type="submit" class="btn btn-primary">Submit</button>
+  		</div>
+	</div>
+</div>
